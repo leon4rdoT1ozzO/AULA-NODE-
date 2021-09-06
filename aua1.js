4cc4f3132b82da6f8e10ae52a4a173362ed5eefa
leon4rdot1ozzO <leonardotiozzob@gmail.com>
2021-09-06 17:08:52 -0300

    foi criado u, arquivo javascrpit

---
+++ aua1.js
@@ -0,0 +1,14 @@
+Nome = "leonardo"
+Idade = 29
+Telefone = "11964406171"
+Endereço = "Eurico"
+Numero_da_Casa = 96440671
+Cidade = "São Paulo"
+Cep = 06240000
+Cpf = "408.5795.98-83"
+Rg = 123456789
+
+
+
+console.log("O nome do contato é " + Nome + " ele tem "  + Idade + "anos de idade, o  numero de  contato dele é " + Telefone + " mora no endereço " + Endereço + " com o numero " + Numero_da_Casa + " da residência, na cidade " + Cidade +  " com o cep " + Cep)
+console.log(" Numeoro do cpf " + Cpf + " e Rg " + Rg)
